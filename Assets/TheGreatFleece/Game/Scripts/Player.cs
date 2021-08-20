2aaceb652fc0af1acc6f73341095476fb184179a
Calum Slee <calumslee@hotmail.co.nz>
2021-08-20 13:04:33 +1200

    Player model added and animated

--- Assets/TheGreatFleece/Game/Scripts/Player.cs
+++ Assets/TheGreatFleece/Game/Scripts/Player.cs
@@ -5,15 +5,35 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    private Vector3 _destination;
+
     private NavMeshAgent _playerNMAgent;
+    private Animator _anim;
 
     private void Start()
     {
+        GetReferences();
+    }
+
+    private void GetReferences()
+    {
         _playerNMAgent = GetComponent<NavMeshAgent>();
 
+        _anim = GetComponentInChildren<Animator>();
+
+        NullCheck();
+    }
+
+    private void NullCheck()
+    {
         if (_playerNMAgent == null)
         {
             Debug.LogError("NavMesh Agent is NULL!");
+        }
+
+        if (_anim == null)
+        {
+            Debug.LogError("Player Animator is NULL!");
         }
     }
 
@@ -24,10 +44,18 @@ public class Player : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
-                _playerNMAgent.SetDestination(hit.point);
+                _destination = hit.point;
+                _playerNMAgent.SetDestination(_destination);
+                _anim.SetBool("Walk", true);
             }
+        }
+
+        float _distance = Vector3.Distance(transform.position, _destination);
+        if (_distance < 1)
+        {
+            _anim.SetBool("Walk", false);
         }
     }
 }
