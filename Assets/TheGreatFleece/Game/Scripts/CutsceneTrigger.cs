e3265053dcd4904c002785d3146a864bc6494967
Calum Slee <calumslee@hotmail.co.nz>
2021-08-30 13:40:58 +1200

    Sleeping guard cutscene integrated into gameplay

---
+++ Assets/TheGreatFleece/Game/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _cutScene;
+    [SerializeField]
+    private Vector3 _darrenEndPos;
+    [SerializeField]
+    private Vector3 _darrenEndRot;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _cutScene.SetActive(true);
+            other.gameObject.transform.position = _darrenEndPos;
+            other.gameObject.transform.eulerAngles = _darrenEndRot;
+        }
+    }
+}
